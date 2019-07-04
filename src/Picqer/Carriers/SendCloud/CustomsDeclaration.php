32c51b3e0c0384f51c5e63369a99a0f97db9c0f0
Werner Wank <14849239+wernerwa@users.noreply.github.com>
2019-07-04 16:58:59 +0200

    add CustomsDeclaration.php

---
+++ src/Picqer/Carriers/SendCloud/CustomsDeclaration.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Picqer\Carriers\SendCloud;
+
+/**
+ * Class CustomsDeclaration
+ *
+ * @property string $normal_printer
+ *
+ * @package Picqer\Carriers\SendCloud
+ */
+class CustomsDeclaration extends Model
+{
+
+    use Query\FindOne;
+
+    protected $fillable = [
+        'normal_printer',
+    ];
+
+    protected $url = 'labels';
+
+    protected $namespaces = [
+        'singular' => 'customs_declaration',
+        'plural' => 'customs_declarations'
+    ];
+
+    /**
+     * Returns the label content (PDF) in A6 format.
+     *
+     * @return string
+     * @throws SendCloudApiException
+     * @throws \RuntimeException if unable to read or an error occurs while reading.
+     * @throws \Picqer\Carriers\SendCloud\SendCloudApiException
+     */
+    public function fetchContent()
+    {
+        $url = str_replace($this->connection->apiUrl(), '', $this->normal_printer);
+
+        return $this->connection->download($url);
+    }
+}
